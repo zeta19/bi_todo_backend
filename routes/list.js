37e25ff5049b302f376fec8a1eef9f254ee7e8c9
zeta19 <koustavmukherjee19@gmail.com>
2020-09-07 23:42:15 +0530

    list feature added

--- routes/list.js
+++ routes/list.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", (req, res, next) => {});
+router.get("/", async (req, res, next) => {
+  try {
+    let tasks = await req.body.collection("tasks").find({}).toArray();
+    return res.json({ error: false, tasks: tasks });
+  } catch (ex) {
+    console.log(ex);
+    return res.json({ error: true, message: "Something went wrong!" });
+  }
+});
 
 module.exports = router;
